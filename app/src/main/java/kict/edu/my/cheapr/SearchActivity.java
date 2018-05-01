93af70f05086358c2df166238b9efb56bcb8b2d1
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-01 17:22:35 +0800

    update java searchactivity

--- app/src/main/java/kict/edu/my/cheapr/SearchActivity.java
+++ app/src/main/java/kict/edu/my/cheapr/SearchActivity.java
@@ -2,10 +2,13 @@ package kict.edu.my.cheapr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -17,18 +20,25 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class SearchActivity extends AppCompatActivity {
+import kict.edu.my.cheapr.web.RetrieveProductList;
+import kict.edu.my.cheapr.web.WebListener;
+
+public class SearchActivity extends AppCompatActivity implements WebListener {
+
+    private final String TAG = getClass().getName();
 
     SearchView sv;
     ListView lv;
-    ItemAdapter ia;
+    ArrayAdapter<String> ia;
     JSONArray json;
+    ArrayList<String> values;
 
-    String dummy = "[" +
-            "{'id':3,'name':'Daia Toilet Cleaner','category':'household','category_name':'Household','supermarket':'Tesco','date_created':'2018-04-14T13:53:35.009779Z','date_updated':'2018-04-14T14:24:45.228848Z'}," +
-            "{'id':5,'name':'Mamee Monster','category':'snack','category_name':'Snack','supermarket':'Tesco','date_created':'2018-04-15T03:38:45.364446Z','date_updated':'2018-04-15T03:38:45.364490Z'},{'id':6,'name':'Jalin Soy Sauce','category':'ingredient','category_name':'Ingredient','supermarket':'hello world','date_created':'2018-04-15T03:39:05.213126Z','date_updated':'2018-04-15T03:39:05.213154Z'},{'id':7,'name':'Oldtown White Coffee 3 in 1','category':'drink','category_name':'Drink','supermarket':'Tesco','date_created':'2018-04-15T03:43:23.942425Z','date_updated':'2018-04-15T03:43:39.643425Z'},{'id':8,'name':'King Dorry Fish','category':'freshfood','category_name':'Fresh Food','supermarket':'hello world','date_created':'2018-04-15T04:02:34.589793Z','date_updated':'2018-04-15T04:02:34.589836Z'},{'id':9,'name':'Rejoice Shampoo ','category':'toiletries','category_name':'Toiletries','supermarket':'hello world','date_created':'2018-04-15T04:18:11.685498Z','date_updated':'2018-04-15T04:26:58.533399Z'}]";
+    private boolean loading;
+    private String url;
 
-
+//    String dummy = "[" +
+//            "{'id':3,'name':'Daia Toilet Cleaner','category':'household','category_name':'Household','supermarket':'Tesco','date_created':'2018-04-14T13:53:35.009779Z','date_updated':'2018-04-14T14:24:45.228848Z'}," +
+//            "{'id':5,'name':'Mamee Monster','category':'snack','category_name':'Snack','supermarket':'Tesco','date_created':'2018-04-15T03:38:45.364446Z','date_updated':'2018-04-15T03:38:45.364490Z'},{'id':6,'name':'Jalin Soy Sauce','category':'ingredient','category_name':'Ingredient','supermarket':'hello world','date_created':'2018-04-15T03:39:05.213126Z','date_updated':'2018-04-15T03:39:05.213154Z'},{'id':7,'name':'Oldtown White Coffee 3 in 1','category':'drink','category_name':'Drink','supermarket':'Tesco','date_created':'2018-04-15T03:43:23.942425Z','date_updated':'2018-04-15T03:43:39.643425Z'},{'id':8,'name':'King Dorry Fish','category':'freshfood','category_name':'Fresh Food','supermarket':'hello world','date_created':'2018-04-15T04:02:34.589793Z','date_updated':'2018-04-15T04:02:34.589836Z'},{'id':9,'name':'Rejoice Shampoo ','category':'toiletries','category_name':'Toiletries','supermarket':'hello world','date_created':'2018-04-15T04:18:11.685498Z','date_updated':'2018-04-15T04:26:58.533399Z'}]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,81 +46,16 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        lv = (ListView)findViewById(R.id.itemSearch);
+        sv = (SearchView)findViewById(R.id.searchField);
+
+        values = new ArrayList<>();
+        loading = false;
+        url = String.format("%s/product/search", WebListener.API);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        try {
-            Log.e("SEARCH", "asfa");
-            json = new JSONArray(dummy);
-            Log.e("SEARCH", json.toString());
-        } catch (JSONException e) {
-            Log.e("SEARCH", "Failed to parse");
-        }
-
-        final ArrayList<String> values = new ArrayList<>();
-        String key = getIntent().getExtras().getString("searchkeyword");
-        Log.e("msg", getIntent().hasExtra("category")?getIntent().getExtras().getString("category"):"NO");
-        lv = (ListView)findViewById(R.id.itemSearch);
-
-
-        sv = (SearchView)findViewById(R.id.searchField);
-        if (getIntent().hasExtra("category")) {
-            sv.setVisibility(View.GONE);//hide the searchview
-            String cat = getIntent().getExtras().getString("category");
-            try {
-                for (int i = 0; i < json.length(); i++) {
-                    JSONObject obj = json.getJSONObject(i);
-                    if (obj.getString("category").equals(cat))
-                        values.add(obj.get("name").toString());
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            ia = new ItemAdapter(this, R.layout.listview_item, values.toArray(new String[0]), "");
-        }
-        else {
-            sv.setQuery(key, true);
-            try {
-                for (int i = 0; i < json.length(); i++) {
-                    JSONObject obj = json.getJSONObject(i);
-                    values.add(obj.get("name").toString());
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            ia = new ItemAdapter(this, R.layout.listview_item, values.toArray(new String[0]), key);
-        }
-        lv.setAdapter(ia);
-//        lv.setAdapter(new BaseAdapter() {
-//            @Override
-//            public int getCount() {
-//                return json.length();
-//            }
-//
-//            @Override
-//            public Object getItem(int i) {
-//                return "item " + i;
-//            }
-//
-//            @Override
-//            public long getItemId(int i) {
-//                return i;
-//            }
-//
-//            @Override
-//            public View getView(int i, View view, ViewGroup viewGroup) {
-//                if(view == null){
-//                    view = new TextView(SearchActivity.this);
-//                    view.setPadding(10,10,10,10);
-//                    ((TextView)view).setTextColor(Color.BLUE);
-//                }
-//
-//                view.setBackgroundColor((i == 1) ?
-//                        Color.argb(0x80, 0x20, 0xa0, 0x40) : Color.argb(0, 0, 0, 0));
-//                ((TextView)view).setText((String)getItem(i));
-//                return view;
-//            }
-//        });
+        retrieveProductListFromWeb();
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -131,13 +76,26 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+                if (!absListView.canScrollList(View.SCROLL_AXIS_VERTICAL)
+                        && scrollState == SCROLL_STATE_IDLE) {
+                    Parcelable parcelable = lv.onSaveInstanceState();
+                    retrieveProductListFromWeb();
+                    if (!loading) lv.onRestoreInstanceState(parcelable);
+                }
+            }
 
-
+            @Override
+            public void onScroll(AbsListView absListView, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {}
+        });
 
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                ia.setKey(s);
+//                ia.setKey(s);
                 lv.invalidateViews();
                 return true;
             }
@@ -153,5 +111,57 @@ public class SearchActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public String onWebSuccess(String response) {
+        Log.d(TAG, "onwebsuccess");
+        Log.d(TAG, response);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            url = jsonObject.getString("next");
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+            Log.d(TAG, jsonArray.toString());
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                if (i < 10) Log.d(TAG, object.toString());
+                values.add(object.getString("name"));
+            }
+            if (lv.getAdapter() == null) {
+                ia = new ArrayAdapter<>(this, R.layout.listview_item, R.id.tv, values);
+                lv.setAdapter(ia);
+            }
+            else {
+                ia.notifyDataSetChanged();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String onWebFailure(String response) {
+        return null;
+    }
+
+    private void retrieveProductListFromWeb() {
+        if (loading) return;
+        loading = true;
+        RetrieveProductList retrieveProductList = new RetrieveProductList();
+        retrieveProductList.setWebListener(this);
+        if (!values.isEmpty())
+            retrieveProductList.execute(String.format("%s", url));
+        else if (getIntent().hasExtra("category")) {
+           sv.setVisibility(View.GONE);
+           String cat = getIntent().getExtras().getString("category");
+           retrieveProductList.execute(String.format("%s?category=%s", url, cat));
+        }
+        else {
+            String key = getIntent().getExtras().getString("searchkeyword");
+            sv.setQuery(key, true);
+            retrieveProductList.execute(String.format("%s?name=%s", url, key));
+        }
+        loading = false;
     }
 }
